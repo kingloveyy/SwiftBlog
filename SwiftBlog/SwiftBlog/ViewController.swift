d29005d1388aaf2965e6a2b80d8793da23c77bca
SK Wang <shoukaiwang@gmail.com>
2015-03-09 00:38:04 +0800

    innit

---
+++ SwiftBlog/SwiftBlog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftBlog
+//
+//  Created by King on 15/3/9.
+//  Copyright (c) 2015年 king. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
